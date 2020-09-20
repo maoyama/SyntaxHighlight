f9635d9f3b39534e84f183555274867199af76a3
Makoto Aoyama <m@aoyama.dev>
2020-09-20 11:07:06 +0900

    Initial Commit

---
+++ SyntaxHighlight/SyntaxHighlight.h
@@ -0,0 +1,19 @@
+//
+//  SyntaxHighlight.h
+//  SyntaxHighlight
+//
+//  Created by Makoto Aoyama on 2020/09/20.
+//  Copyright © 2020 dev.aoyama. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SyntaxHighlight.
+FOUNDATION_EXPORT double SyntaxHighlightVersionNumber;
+
+//! Project version string for SyntaxHighlight.
+FOUNDATION_EXPORT const unsigned char SyntaxHighlightVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SyntaxHighlight/PublicHeader.h>
+
+
